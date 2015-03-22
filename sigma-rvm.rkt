f4cedab982026e1133948df9f20f7053349b5c0a
Tero Hasu <tero@ii.uib.no>
2015-03-22 13:39:24 +0200

    add `when` and `unless` with `#:cleanup` clause

--- sigma-rvm.rkt
+++ sigma-rvm.rkt
@@ -15,11 +15,11 @@ conditional forms in this language.
   (require "i1-internal.rkt"
            (for-syntax racket/base syntax/parse))
   
-  (provide my-if)
+  (provide my-if my-when my-unless)
 
   (define-syntax (monadic-if/cleanup stx)
     (syntax-parse stx
-      [(_ c:expr t:expr e:expr (~optional (~seq #:cleanup cact ...)))
+      [(_ c:expr t:expr e:expr (~optional (~seq #:cleanup cact:expr ...)))
        (with-syntax ([(act ...) (if (attribute cact)
                                     (syntax->list #'(cact ...))
                                     null)])
@@ -29,12 +29,28 @@ conditional forms in this language.
                      (bad-condition #:bad-arg v #'monadic-if/cleanup)
                    act ...)
                  (if (Good-v v) t e))))]))
-  
+
+  (define-syntax (my-when stx)
+    (syntax-parse stx
+      [(_ c:expr ts:expr ...+ #:cleanup cact:expr ...)
+       #'(my-if c (begin ts ...) (void) #:cleanup cact ...)]
+      [(_ c:expr ts:expr ...+)
+       #'(my-if c (begin ts ...) (void))]
+      ))
+
+  (define-syntax (my-unless stx)
+    (syntax-parse stx
+      [(_ c:expr ts:expr ...+ #:cleanup cact:expr ...)
+       #'(my-if c (void) (begin ts ...) #:cleanup cact ...)]
+      [(_ c:expr ts:expr ...+)
+       #'(my-if c (void) (begin ts ...))]
+      ))
+
   (define-my-syntax my-if monadic-if/cleanup if))
 
 (require "i1-lang.rkt" "i1-lib.rkt" (submod "." impl))
 (provide (except-out (all-from-out "i1-lang.rkt") 
                      if if-not or and cond block)
          (all-from-out "i1-lib.rkt")
-         (rename-out [my-if if]) set!
-         #%top-interaction)
+         (rename-out [my-if if] [my-when when] [my-unless unless])
+         set! #%top-interaction)
