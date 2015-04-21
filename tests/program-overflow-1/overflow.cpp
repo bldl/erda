0dfad6611be2e35e1401cbc034b6f7c566682eb6
Tero Hasu <tero@ii.uib.no>
2015-04-21 17:51:27 +0300

    fix programs broken due to AlertName switch

--- tests/program-overflow-1/overflow.cpp
+++ tests/program-overflow-1/overflow.cpp
@@ -31,18 +31,18 @@ static Result<int> compute_with(bool (*f)(int, int, int*),
 				Result<int> const& x,
 				Result<int> const& y) {
   if (is_Bad(x)) {
-    return Bad(Nothing<Result<int>>(), mgl_SYMBOL("bad-arg"));
+    return Bad(Nothing<Result<int>>(), AlertName("bad-arg"));
   } else if (is_Bad(y)) {
-    return Bad(Nothing<Result<int>>(), mgl_SYMBOL("bad-arg"));
+    return Bad(Nothing<Result<int>>(), AlertName("bad-arg"));
   } else {
     int r;
     bool err = (*f)(Good_v(x), Good_v(y), &r);
     if (err) {
-      return Bad(Nothing<Result<int>>(), mgl_SYMBOL("div-by-0"));
+      return Bad(Nothing<Result<int>>(), AlertName("div-by-0"));
     } else if (is_data_invariant(r)) {
       return Good(r);
     } else {
-      return Bad(Just(Good(r)), mgl_SYMBOL("data-invariant"));
+      return Bad(Just(Good(r)), AlertName("data-invariant"));
     }
   }
 }
